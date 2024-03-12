cd76a76b286d939278ef393539779e288dea4c9d
Jasmine Lee <jasminelee_0409@hotmail.com>
2024-03-11 23:12:30 -0400

    Issue#11 - Added typeDef

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,20 +1,49 @@
 const typeDefs = `
-  type Profile {
+  type User {
     _id: ID
-    name: String
-    skills: [String]!
+    username: String
+    password: String
+    buddy: ID
+    availability: Boolean
+    role: String
+    expertise: String
+    conversations: [Conversation]!
+  }
+
+  type Conversation {
+    conversationTitle: String
+    conversationText: String
+    expertise: String
+    author: ID
+    listener: ID
+    comments: [Comment]!
+    createdAt: String
+    is_closed: Boolean
+  }
+
+  type Response {
+    commentId: ID
+    comment: String
+    author: String
+    createdAt: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
   }
 
   type Query {
-    profiles: [Profile]!
-    profile(profileId: ID!): Profile
+    users: [User]
+    user(id: ID!): User
+    conversation(id: ID!): Conversation
   }
 
   type Mutation {
-    addProfile(name: String!): Profile
-    addSkill(profileId: ID!, skill: String!): Profile
-    removeProfile(profileId: ID!): Profile
-    removeSkill(profileId: ID!, skill: String!): Profile
+    addUser(username: String!, password: String!, role: String!, expertise: String): Auth
+    login(email: String!, password: String!): Auth
+    addConversation(conversationTitle: String!, conversationText: String!, expertise: String!): Conversation
+    addComment(conversationId: ID!, comment: String!, author: ID!): Conversation
   }
 `;
 
