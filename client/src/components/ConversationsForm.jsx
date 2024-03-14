3fe9586498de8380c9170ab7f44c17f1a3c32ad1
Jasmine Lee <jasminelee_0409@hotmail.com>
2024-03-13 23:19:51 -0400

    Added the textarea

--- client/src/components/ConversationsForm.jsx
+++ client/src/components/ConversationsForm.jsx
@@ -39,6 +39,24 @@ export default function ConversationsForm() {
             </div>
 
             <div>
+              <label
+                htmlFor="conversation-text"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                And some more details...
+              </label>
+              <div className="mt-2">
+                <textarea
+                  rows={4}
+                  name="conversation-text"
+                  id="conversation-text"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  defaultValue={""}
+                />
+              </div>
+            </div>
+
+            <div>
               <button
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
