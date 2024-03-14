7cdfe36202fc22ca48796030bf7721a0eb10b242
Tiffany <tchan12899@gmail.com>
2024-03-14 18:47:48 -0400

    Removed imports from app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,8 +7,6 @@ import Nav from './components/Navbar';
 // import Header from './components/Header';
 // import Footer from './components/Footer';
 
-import Role from './components/Role'
-
 const client = new ApolloClient({
   uri: '/graphql',
   cache: new InMemoryCache(),
@@ -16,10 +14,7 @@ const client = new ApolloClient({
 
 function App() {
   return (
-    <div>
-          <Nav />
-          <Role />
-    </div>
+    <Nav />
 
     // <ApolloProvider client={client}>
     //   <div className="flex-column justify-flex-start min-100-vh">
