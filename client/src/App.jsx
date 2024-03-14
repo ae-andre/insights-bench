842bfa71064f365965d83a8fab807a029a55ca5c
Tiffany <tchan12899@gmail.com>
2024-03-14 00:05:36 -0400

    Centered action panel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,8 @@ import Nav from './components/Navbar';
 // import Header from './components/Header';
 // import Footer from './components/Footer';
 
+import Role from './components/Role'
+
 const client = new ApolloClient({
   uri: '/graphql',
   cache: new InMemoryCache(),
@@ -14,7 +16,11 @@ const client = new ApolloClient({
 
 function App() {
   return (
-    <Nav />
+    <div>
+          <Nav />
+          <Role />
+    </div>
+
     // <ApolloProvider client={client}>
     //   <div className="flex-column justify-flex-start min-100-vh">
     //     <Header />
--- client/src/components/Role/index.jsx
+++ client/src/components/Role/index.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 export default function Example() {
     return (
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 items-center">
       <div className="bg-white shadow sm:rounded-lg">
-        <div className="px-4 py-5 sm:p-6">
-          <h3 className="text-base font-semibold leading-6 text-gray-900">Manage subscription</h3>
-          <div className="mt-2 max-w-xl text-sm text-gray-500">
+        <div className="px-4 py-5 sm:p-6 text-center">
+          <h3 className="text-base font-semibold leading-6 text-gray-900">Choose a seat on the bench!</h3>
+          <div className="mt-2 text-sm text-gray-500 text-center">
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae voluptatibus corrupti atque repudiandae
               nam.
@@ -14,12 +15,19 @@ export default function Example() {
           <div className="mt-5">
             <button
               type="button"
-              className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+              className="inline-flex mx-6 items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
             >
-              Change plan
+              Listener
+            </button>
+            <button
+              type="button"
+              className="inline-flex mx-6 items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+            >
+              Sharer
             </button>
           </div>
         </div>
       </div>
+    </div>
     )
   }
