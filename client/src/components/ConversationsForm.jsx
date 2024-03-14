cfeb20c5bebe6defdea8a01dace5ca1249ec863d
Jasmine Lee <jasminelee_0409@hotmail.com>
2024-03-14 18:41:48 -0400

    Updated the text

--- client/src/components/ConversationsForm.jsx
+++ client/src/components/ConversationsForm.jsx
@@ -7,7 +7,7 @@ export default function ConversationsForm() {
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            What's on your mind?
+          Let's put things to bench
           </h2>
         </div>
 
@@ -19,7 +19,7 @@ export default function ConversationsForm() {
                   for="problem"
                   class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
-                  Field of Interest:
+                  What's on your mind:
                 </label>
                 <select
                   id="problem-area"
