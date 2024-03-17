bd9064f09bb332e3f4b4d6f581f5b694e69c758c
Tiffany <tchan12899@gmail.com>
2024-03-17 01:36:48 -0400

    Added auth files and successfully logged in on local server with seed data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,40 +1,67 @@
 import './App.css';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 import { Outlet } from 'react-router-dom';
+import { setContext } from '@apollo/client/link/context';
+
 
 import Nav from './components/Navbar';
 
 import Dashboard from './pages/Dashboard';
+
+import Login from './pages/Login';
+
+import Footer from './components/Footer'
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
 
 // import ListenerSignup from './components/ListenerSignup'
 
 // import Header from './components/Header';
 // import Footer from './components/Footer';
 
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  uri: '/graphql',
+  // uri: '/graphql',
+  // cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
 function App() {
   return (
-    <div>
-    <Nav />
+    // <div>
+    // <Nav />
 
-    <Dashboard />
-    </div>
+    // <Login />
+    // </div>
 
 
 
-    // <ApolloProvider client={client}>
-    //   <div className="flex-column justify-flex-start min-100-vh">
-    //     <Header />
-    //     <div className="container">
-    //       <Outlet />
-    //     </div>
-    //     <Footer />
-    //   </div>
-    // </ApolloProvider>
+    <ApolloProvider client={client}>
+      <div>
+        <Nav />
+        <div>
+          {/* <Outlet /> */}
+          <Login />
+        </div>
+        <Footer />
+      </div>
+    </ApolloProvider>
   );
 }
 
