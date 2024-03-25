12025a1a75a6432325d38a09785a3a1663f59e28
Tiffany <tchan12899@gmail.com>
2024-03-24 23:08:35 -0400

    Added char count for more details & added more padding

--- client/src/components/ConversationsForm.jsx
+++ client/src/components/ConversationsForm.jsx
@@ -124,8 +124,9 @@ const ConversationsForm = (props) => {
                   value={convoForm.conversationTitle}
                   onChange={handleChange}
                   placeholder="  Topic of the discussion"
-                  className="input-option block w-full rounded-md border-1 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="pl-2 input-option block w-full rounded-md border-1 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
+                <span className="pt-2 inset-y-0 right-0 pr-2 flex items-center text-sm text-gray-400">{convoForm.conversationTitle.length}/50</span>
               </div>
             </div>
 
@@ -144,8 +145,9 @@ const ConversationsForm = (props) => {
                   placeholder="  Your story goes here.."
                   value={convoForm.conversationText}
                   onChange={handleChange}
-                  className="input-option block w-full rounded-md border-1 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="mb-0 input-option block w-full rounded-md border-1 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
+                <span className="pt-2 inset-y-0 right-0 pr-2 flex items-center text-sm text-gray-400">{convoForm.conversationText.length}/500</span>
               </div>
             </div>
 
